974b8b317826653df42a74264b066fb2c39b5ee0
SUGIYAMA Yoshio <nenegi.01mo@gmail.com>
2023-02-09 12:09:42 +0900

    feat: baseline.
    Signed-off-by: SUGIYAMA Yoshio <nenegi.01mo@gmail.com>

---
+++ src/config.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'IMOKURI Blog';
+export const SITE_DESCRIPTION = 'Tech blog by SUGIYAMA Yoshio who is an infrastructure / ML engineer';
---
+++ tailwind.config.cjs
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        latte: {
+          primary: "#8839ef", // Mauve
+          secondary: "#ea76cb", // Pink
+          accent: "#179299", // Teal
+          neutral: "#4c4f69", // Text
+          "base-100": "#eff1f5", // Base
+          info: "#04a5e5", // Sky
+          success: "#40a02b", // Green
+          warning: "#df8e1d", // Yellow
+          error: "#e64553", // Maroon
+        },
+        frappe: {
+          primary: "#ca9ee6", // Mauve
+          secondary: "#f4b8e4", // Pink
+          accent: "#81c8be", // Teal
+          neutral: "#c6d0f5", // Text
+          "base-100": "#303446", // Base
+          info: "#99d1db", // Sky
+          success: "#a6d189", // Green
+          warning: "#e5c890", // Yellow
+          error: "#ea999c", // Maroon
+        },
+      },
+    ],
+    darkTheme: "frappe",
+  },
+};
