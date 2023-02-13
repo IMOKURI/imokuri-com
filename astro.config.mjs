76325e8d4181774172e69742d7f3ceea59218215
SUGIYAMA Yoshio <nenegi.01mo@gmail.com>
2023-02-13 14:18:38 +0900

    feat: Add sitemap.
    Signed-off-by: SUGIYAMA Yoshio <nenegi.01mo@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://imokuri.com',
-  integrations: [tailwind()]
+    site: "https://imokuri.com",
+    integrations: [tailwind(), sitemap()],
 });
