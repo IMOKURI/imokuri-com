e2f4cadd18d513e9bb196449974d9ceddfb9428c
SUGIYAMA Yoshio <nenegi.01mo@gmail.com>
2023-02-16 11:20:05 +0900

    fix: Update RSS feed.
    Signed-off-by: SUGIYAMA Yoshio <nenegi.01mo@gmail.com>

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,24 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
+import { getCollection } from "astro:content";
 
 export async function get() {
-    return rss({
-        title: SITE_TITLE,
-        description: SITE_DESCRIPTION,
-        site: import.meta.env.SITE,
-        items: await pagesGlobToRssItems(import.meta.glob("./blog/**/*.md")),
-    });
+  const blog = (await getCollection("blog"))
+    .sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf())
+    .slice(0, 5);
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: import.meta.env.SITE,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.date,
+      link: `/blog/${post.data.date.getFullYear()}/${(
+        post.data.date.getMonth() + 1
+      )
+        .toString()
+        .padStart(2, "0")}/${post.slug}/`,
+    })),
+  });
 }
