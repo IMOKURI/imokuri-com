f9258eb9192ce14cb7afe2dce23fb68c3ed6a774
SUGIYAMA Yoshio <nenegi.01mo@gmail.com>
2024-11-01 10:07:56 +0900

    feat: update profile
    Signed-off-by: SUGIYAMA Yoshio <nenegi.01mo@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = 'IMOKURI Blog';
-export const SITE_DESCRIPTION = 'Tech blog by SUGIYAMA Yoshio who is an infrastructure / ML engineer';
+export const SITE_TITLE = 'IMOKURING';
+export const SITE_DESCRIPTION = 'Personal website by Yoshio Sugiyama who is an infrastructure / ML engineer';
