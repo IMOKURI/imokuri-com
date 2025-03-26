7db87d099eb542582bdd81e731bb4a4dc3a2773d
SUGIYAMA Yoshio <nenegi.01mo@gmail.com>
2025-03-26 12:55:19 +0900

    feat: use tailwindcss/vite
    Signed-off-by: SUGIYAMA Yoshio <nenegi.01mo@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,20 @@ import { defineConfig } from "astro/config"
 import sitemap from "@astrojs/sitemap"
 import mdx from "@astrojs/mdx"
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://imokuri.com",
   integrations: [sitemap(), mdx()],
+
   markdown: {
     shikiConfig: {
       theme: "nord"
     }
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
   }
 })
