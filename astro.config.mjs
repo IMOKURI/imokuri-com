969e98a405a5316e55d7ccea18dec611f0028ab6
SUGIYAMA Yoshio <nenegi.01mo@gmail.com>
2025-03-26 12:31:16 +0900

    deps: astro
    Signed-off-by: SUGIYAMA Yoshio <nenegi.01mo@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import mdx from "@astrojs/mdx";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
+import mdx from "@astrojs/mdx"
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://imokuri.com",
-    integrations: [tailwind(), sitemap(), mdx()],
-    markdown: {
-        shikiConfig: {
-            theme: "nord",
-        },
-    },
-});
+  site: "https://imokuri.com",
+  integrations: [tailwind(), sitemap(), mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: "nord"
+    }
+  }
+})
