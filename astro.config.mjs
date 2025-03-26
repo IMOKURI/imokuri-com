907fbb7a6bba739a5faba77a45655678d90b3730
SUGIYAMA Yoshio <nenegi.01mo@gmail.com>
2025-03-26 12:47:40 +0900

    refactor: remove astrojs/tailwind
    Signed-off-by: SUGIYAMA Yoshio <nenegi.01mo@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
 import sitemap from "@astrojs/sitemap"
 import mdx from "@astrojs/mdx"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://imokuri.com",
-  integrations: [tailwind(), sitemap(), mdx()],
+  integrations: [sitemap(), mdx()],
   markdown: {
     shikiConfig: {
       theme: "nord"
