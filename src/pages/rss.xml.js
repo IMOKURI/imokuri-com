eca7fdeb74114637d48d70f840a14327f3f69ca0
SUGIYAMA Yoshio <nenegi.01mo@gmail.com>
2025-10-01 19:43:41 +0900

    refactor: formatting
    Signed-off-by: SUGIYAMA Yoshio <nenegi.01mo@gmail.com>

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,24 +1,20 @@
-import rss from "@astrojs/rss";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
-import { getCollection } from "astro:content";
+import rss from "@astrojs/rss"
+import { SITE_TITLE, SITE_DESCRIPTION } from "../config"
+import { getCollection } from "astro:content"
 
 export async function GET() {
-  const blog = (await getCollection("blog"))
-    .sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf())
-    .slice(0, 5);
+  const blog = (await getCollection("blog")).sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf()).slice(0, 5)
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
-    items: blog.map((post) => ({
+    items: blog.map(post => ({
       title: post.data.title,
       description: post.data.description,
       pubDate: post.data.date,
-      link: `/blog/${post.data.date.getFullYear()}/${(
-        post.data.date.getMonth() + 1
-      )
+      link: `/blog/${post.data.date.getFullYear()}/${(post.data.date.getMonth() + 1)
         .toString()
-        .padStart(2, "0")}/${post.id}/`,
-    })),
-  });
+        .padStart(2, "0")}/${post.id}/`
+    }))
+  })
 }
